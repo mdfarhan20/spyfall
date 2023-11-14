3a3ae8f3de35e474c3a60f7624ad8f1acd42d336
Mohamed Farhan <mohamedfarhan201103@gmail.com>
2023-11-14 14:34:36 +0530

    location hide feature added

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -5,7 +5,7 @@ function GameCard({ location }) {
     const [hidden, setHidden] = useState(false);
 
     return (
-        <div className="grid gap-y-4 p-4 rounded-lg bg-yellow-100 ">
+        <div className="grid gap-y-4 p-4 rounded-lg bg-yellow-100" onClick={() => setHidden(!hidden)}>
             { hidden && <img src={hide.src} className="block w-full max-w-full rounded-lg" /> }
             { !hidden && <img src={location.src} className="block w-full max-w-full rounded-lg" /> }
             <p className="text-center text-2xl tracking-wider font-semibold py-2">{ hidden ? hide.name : location.name }</p>
